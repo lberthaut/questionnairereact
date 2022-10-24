0cf0791a97ed025fa0f1168676043265025251d8
Louis <louis.berthaut@gmx.com>
2022-10-24 14:14:48 +0200

    beginning

---
+++ questionnaire/src/app/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import '../styles/App.css';
+import Header from '../components/header'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ questionnaire/src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/headerstyle.css'
+
+export default function Header (){
+    return(
+        <div className="header_bar">
+            <h1>Questionnaire</h1>
+        </div>
+    )
+}
